5ff1a01a97dfcbf7bc5d63ed07755328e7fe55ff
Martín Rosso <mrosso10@gmail.com>
2024-07-24 10:34:06 -0300

    test: remove outdated test
    Now that API controllers inherits from ActionController::API
    the default format for requests is JSON

--- spec/controllers/api/todo_lists_controller_spec.rb
+++ spec/controllers/api/todo_lists_controller_spec.rb
@@ -39,32 +39,22 @@ describe Api::TodoListsController do
   describe 'GET index' do
     let!(:todo_list) { TodoList.create(name: 'Setup RoR project') }
 
-    context 'when format is HTML' do
-      it 'raises a routing error' do
-        expect do
-          get :index
-        end.to raise_error(ActionController::RoutingError, 'Not supported format')
-      end
+    it 'returns a success code' do
+      get :index, format: :json
+
+      expect(response).to have_http_status(:ok)
     end
 
-    context 'when format is JSON' do
-      it 'returns a success code' do
-        get :index, format: :json
+    it 'includes todo list records' do
+      get :index, format: :json
 
-        expect(response).to have_http_status(:ok)
-      end
+      todo_lists = response.parsed_body
 
-      it 'includes todo list records' do
-        get :index, format: :json
-
-        todo_lists = response.parsed_body
-
-        aggregate_failures 'includes the id and name' do
-          expect(todo_lists.count).to eq(1)
-          expect(todo_lists[0].keys).to match_array(%w[id name])
-          expect(todo_lists[0]['id']).to eq(todo_list.id)
-          expect(todo_lists[0]['name']).to eq(todo_list.name)
-        end
+      aggregate_failures 'includes the id and name' do
+        expect(todo_lists.count).to eq(1)
+        expect(todo_lists[0].keys).to match_array(%w[id name])
+        expect(todo_lists[0]['id']).to eq(todo_list.id)
+        expect(todo_lists[0]['name']).to eq(todo_list.name)
       end
     end
   end
