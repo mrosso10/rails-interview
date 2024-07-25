30b58a10364343e10b51fd1f3d122528144602f0
Martín Rosso <mrosso10@gmail.com>
2024-07-25 10:45:29 -0300

    feat: allow turbo stream for links
    also, all extra keyword arguments are bypassed from link

--- app/components/link_component.rb
+++ app/components/link_component.rb
@@ -3,17 +3,29 @@
 class LinkComponent < ViewComponent::Base
   BUTTON_CLASS = 'inline-block rounded py-2 px-3 text-white font-medium'
 
-  def initialize(text:, target:, type: :primary, method: :get)
+  def initialize(
+    text:,
+    target:,
+    type: :primary,
+    method: :get,
+    turbo_stream: false
+  )
     @text = text
     @target = target
     @type = type
     @method = method
+    @turbo_stream = turbo_stream
 
     super
   end
 
   def call
-    link_to @text, @target, 'data-turbo-method': @method, class: "#{BUTTON_CLASS} #{background_color}"
+    options = {
+      'data-turbo-method': @method,
+      class: "#{BUTTON_CLASS} #{background_color}"
+    }
+    options[:'data-turbo-stream'] = true if @turbo_stream
+    link_to @text, @target, options
   end
 
   private
--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,6 +1,6 @@
 module LinksHelper
-  def link(text, target, type: :primary)
-    render LinkComponent.new(text:, target:, type:)
+  def link(text, target, type: :primary, **)
+    render LinkComponent.new(text:, target:, type:, **)
   end
 
   def link_delete(text, target, type: :danger)
