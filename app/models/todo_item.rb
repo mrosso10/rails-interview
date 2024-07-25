8adf74bf85b2b9aaae4480b9fc1d8326aecfb52e
Martín Rosso <mrosso10@gmail.com>
2024-07-25 17:17:18 -0300

    fix: update items in realtime
     #turbo-streams #actioncable

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -2,4 +2,16 @@ class TodoItem < ApplicationRecord
   belongs_to :todo_list
 
   validates :description, presence: true
+
+  after_create_commit do
+    broadcast_append_to todo_list
+  end
+
+  after_update_commit do
+    broadcast_update_to todo_list
+  end
+
+  after_destroy do
+    broadcast_remove_to todo_list
+  end
 end
